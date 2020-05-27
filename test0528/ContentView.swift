1b87237f7df279e830a4d5be8d9208456d6dd927
SandyLee1999 <4732.girl.sandy@gmail.com>
2020-05-28 00:56:00 +0800

    commit_edit_test

---
+++ test0528/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  test0528
+//
+//  Created by Sandy on 2020/5/28.
+//  Copyright © 2020 Sandy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        
+    }
+}
+
+
+
+
+
